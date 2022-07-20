5b8f5490e0d095ccd9edfcdbfe967755ae33f6dd
Alex Belyaev <t433621@yandex.ru>
2022-07-20 21:02:42 +0300

    add text

--- for/Program.cs
+++ for/Program.cs
@@ -1,5 +1,5 @@
-﻿string Method4(int count, string text)
-{
+﻿//string Method4(int count, string text)
+/*{
     string result = String.Empty;
     for (int i = 0; i < count; i++)
 {
@@ -15,7 +15,37 @@ for(int i=2; i <=10; i++)
 {
     for(int j=2; j<=10; j++)
     {
-        Console.WriteLine($"{i}x{j}={i*j}");
+    //    Console.WriteLine($"{i}x{j}={i*j}");
     }
-    Console.WriteLine();
+//Console.WriteLine();
+}*/
+
+
+// Обработка текста
+// Пробелы заменить на | маленькие "к" на большие "К" большие "С" на маленькие "с"
+
+string text = "Я думаю Саша клякса князь"
+            +"кот Суп Скрипка";
+// string s ="qwerty"
+//            0123
+// s[3] r
+
+string Replace(string text, char old, char new)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for(int i=0; i<length; i++)
+    {
+        if(text[i] == old) {result = result +$"{new}";}
+        else
+           {
+            result = result + $"{text{i]}";
+            }
+    }
+            return result;
+    
 }
+string newText = Replace(text, ' ', '|');
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
