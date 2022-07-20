94aa41a323a08d0b7a61f1c092595df4c4534312
Alex Belyaev <t433621@yandex.ru>
2022-07-20 10:39:59 +0300

    add 2x2

--- for/Program.cs
+++ for/Program.cs
@@ -8,4 +8,14 @@
 return result;
 }
 string res = Method4(3, "asdf");
-Console.WriteLine(res);
+//Console.WriteLine(res);
+
+// Вложенные цыклы на примере таблицы умножения
+for(int i=2; i <=10; i++)
+{
+    for(int j=2; j<=10; j++)
+    {
+        Console.WriteLine($"{i}x{j}={i*j}");
+    }
+    Console.WriteLine();
+}
