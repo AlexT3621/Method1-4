2a37712d64460d3bd82770e0af9ae092f1c4b8d1
Alex Belyaev <t433621@yandex.ru>
2022-07-21 09:27:44 +0300

    add MinMAX

---
+++ SortirovkaMinMax/Program.cs
@@ -0,0 +1,37 @@
+﻿// Найти позицию минимального элемента в несортированной части массива
+// Произвести обмен этого значения со значением первой несортиоанной позиции
+// Повторять пока есть неотсортированные элементы
+
+int[] arr = {1,5,4,3,2,6,7,1,1};
+void PrintArray(int[] array) // метод печати массива
+{
+    int count = array.Length;
+    for(int i = 0; i<count; i++)
+    {
+        Console.Write(array[i]);
+    }
+}
+Console.WriteLine();
+void Sort(int[] array)
+{
+    for(int i = 0; i < array.Length-1; i++)
+    {
+        int minPosition = i;
+        for(int j = i +1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+                int temp = array[i];
+                array[i] = array[minPosition];
+                array[minPosition] = temp;  
+                      
+    }           
+    
+}
+
+PrintArray(arr);
+Sort(arr);
+Console.WriteLine();
+PrintArray(arr);
+    
--- for/Program.cs
+++ for/Program.cs
@@ -30,22 +30,31 @@ string text = "Я думаю Саша клякса князь"
 //            0123
 // s[3] r
 
-string Replace(string text, char old, char new)
+string Replace(string text, char old1, char new1)
 {
     string result = String.Empty;
     int length = text.Length;
     for(int i=0; i<length; i++)
     {
-        if(text[i] == old) {result = result +$"{new}";}
-        else
-           {
-            result = result + $"{text{i]}";
-            }
-    }
-            return result;
+        if(text[i] == old1)      
+            result = result +$"{new1}";
+        
+        else 
+              
+            result = result + text[i];
+            
+    }  
+            
+            
     
-}
+            return result;
+}  
+
 string newText = Replace(text, ' ', '|');
-Console.WriteLine();
-newText = Replace(text, 'к', 'К');
 Console.WriteLine(newText);
+ newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
+
